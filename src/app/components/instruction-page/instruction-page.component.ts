abe1ff8f2b5a08dd40a33ebe0e48b66d46f66945
Anastasiia <troyanovskaya2003@gmail.com>
2024-06-13 14:50:54 +0100

    instruction added

--- src/app/components/instruction-page/instruction-page.component.ts
+++ src/app/components/instruction-page/instruction-page.component.ts
@@ -6,6 +6,23 @@ import { Component } from '@angular/core';
   styleUrls: ['./instruction-page.component.css']
 })
 export class InstructionPageComponent {
-  label = "Інструкція";
-  text = [];
+  label = 'Інструкція';
+  text = [{item: 'Для того, щоб перейти на головну сторінку зі словником треба натиснути на «Словник» на панелі навігації.',
+    img: ['assets/img/img1.jpg', 'assets/img/img4.jpg']},
+    {item: 'Щоб перейти на словникову статтю, натисніть на слово або одну з його семем.',
+      img: ['assets/img/img7.jpg', 'assets/img/img2.jpg']
+    },
+    {item: 'Щоб знайти ціле слово за підрядком, введіть його у пошуковий рядок.',
+      img: ['assets/img/img6.jpg']
+    },
+    {item: 'Щоб знайти всі слова, що починаються на певну літеру, натисніть на літеру із переліку.',
+      img: ['assets/img/img5.jpg']
+    },
+    {item: 'Щоб знайти знайти всі слова, що входять до лексико-тематичної групи чи підгрупи, оберіть один з елементів розкривного списку.',
+      img: ['assets/img/img9.jpg', 'assets/img/img8.jpg', 'assets/img/img3.jpg']
+    },
+    {item: 'Всі види пошуку можна використовувати одночасно, для підвищення точності результатів.',
+      img: ['assets/img/img10.jpg']
+    }
+  ];
 }
